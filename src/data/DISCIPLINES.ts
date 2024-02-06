27e056c2193cae35c170bf5bea49d188a6c49562
Jacob Blazina <jacob@rivialsecurity.com>
2024-02-05 20:23:38 -0800

    fix: fixed icons

--- src/data/DISCIPLINES.ts
+++ src/data/DISCIPLINES.ts
@@ -4,55 +4,55 @@ export const DISCIPLINES = [
         name: "Product Management",
         description:
             "Collaborating with internal and external stakeholders to define product vision, strategy, and roadmap. Being a subject matter expert for the product and bridging between business people and technical teams.",
-        icon: "category-icon",
+        icon: "material-symbols:category-outline",
     },
     {
         id: 1,
         name: "Software Development Management",
         description:
             "Managing a team of software engineers, designers, and testers. Planning and executing software development projects. Managing budgets, timelines, and resources.",
-        icon: "groups-icon",
+        icon: "material-symbols:groups-outline",
     },
     {
         id: 2,
         name: "Software Engineering",
         description:
             "Building software applications to solve real problems. Writing code, designing user interfaces, and architecting cloud infrastructure. Working with other engineers to build large scale software systems.",
-        icon: "engineering-icon",
+        icon: "material-symbols:engineering-outline",
     },
     {
         id: 3,
         name: "Quality Assurance",
         description:
             "Testing software and features to ensure that they meet the requirements and are free of bugs. Writing automated tests to ensure that software continues to work as it is updated. Maintaining world-class quality standards.",
-        icon: "domain-verification-icon",
+        icon: "material-symbols:domain-verification-outline",
     },
     {
         id: 4,
         name: "User Support",
         description:
             "Helping users to understand how to use software applications. Troubleshooting issues and providing solutions. Working with users to understand their needs and how to improve the software.",
-        icon: "support-agent-icon",
+        icon: "material-symbols:support-agent",
     },
     {
         id: 5,
         name: "DevOps",
         description:
             "Building and maintaining cloud infrastructure to support software applications. Automating the deployment of software applications. Monitoring and maintaining the health of software applications.",
-        icon: "storage-icon",
+        icon: "material-symbols:storage",
     },
     {
         id: 6,
         name: "User Experience Design",
         description:
             "Designing user interfaces and user experiences to ensure that software applications are easy to use and intuitive. Working with users to understand their needs and how to improve the software.",
-        icon: "preview-icon",
+        icon: "material-symbols:preview-outline",
     },
     {
         id: 7,
         name: "Cybersecurity",
         description:
             "Ensuring that software applications are secure and that user data is protected. Working with security teams to ensure that software applications meet security standards.",
-        icon: "gpp-good-icon",
+        icon: "material-symbols:security",
     },
 ];
--- src/data/INTERESTS.ts
+++ src/data/INTERESTS.ts
@@ -4,53 +4,53 @@ export const INTERESTS = [
         name: "Family",
         description:
             "Spending time with my wife and kids, we operate as a team to support each other.",
-        icon: "family-restroom-icon",
+        icon: "material-symbols:family-restroom",
     },
     {
         id: 1,
         name: "Power Sports",
         description: "I love to ride motorcycles, ATVs, and Side by Sides.",
-        icon: "two-wheeler-icon",
+        icon: "material-symbols:two-wheeler",
     },
     {
         id: 2,
         name: "Camping",
         description:
             "Tent camping, hammock camping, or in a trailer; I love the outdoors and the feeling of being self-sufficient.",
-        icon: "forest-icon",
+        icon: "material-symbols:forest",
     },
     {
         id: 3,
         name: "3D Printing",
         description:
             "I'm a creator at heart, and 3D printing is a great way to use my development skills to create physical objects.",
-        icon: "view-in-ar-icon",
+        icon: "material-symbols:view-in-ar",
     },
     {
         id: 4,
         name: "Gaming",
         description:
             "I play games with my kids my friends. I'm a big fan of strategy, racing, and RPG games. I have a recent passion for VR racing and flight simulators",
-        icon: "gaming-icon",
+        icon: "streamline:ai-gaming-spark",
     },
     {
         id: 5,
         name: "Music",
         description:
             "I grew up playing trumpet in school bands. As an adult, I've very slowly been learning to play piano which has been challenging and fun.",
-        icon: "piano-icon",
+        icon: "material-symbols:piano",
     },
     {
         id: 6,
         name: "Fishing",
         description:
             "They don't call it 'catching' for a reason. I primarily fish high lakes for rainbow and brook trout",
-        icon: "phishing-icon",
+        icon: "material-symbols:phishing",
     },
     {
         id: 7,
         name: "Pets",
         description: "Dogs, cats, horses, goats, and chickens. They're all great.",
-        icon: "pets-icon",
+        icon: "material-symbols:pets",
     },
 ];
